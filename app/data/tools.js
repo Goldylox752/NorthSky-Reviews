d8d7aa38ad2dae20898a9489778de0612ab2b619
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-27 11:19:40 -0600

    Create tools.js

---
+++ app/data/tools.js
@@ -0,0 +1,36 @@
+export const categories = [
+
+{
+slug:"ai-writing",
+name:"AI Writing",
+icon:"✍️"
+},
+
+{
+slug:"vpn",
+name:"VPN",
+icon:"🔒"
+},
+
+{
+slug:"esim",
+name:"eSIM",
+icon:"🌎"
+},
+
+{
+slug:"ai-business",
+name:"Business AI",
+icon:"💼"
+},
+
+{
+slug:"automation",
+name:"Automation",
+icon:"⚙️"
+}
+
+];
+
+
+export default tools;
