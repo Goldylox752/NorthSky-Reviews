041c1e6e4b26e9dca17f20d0aef401a79f7db3b2
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-07-30 13:09:32 -0600

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,30 +1,50 @@
-export default function robots() {
+const baseUrl =
+  "https://northsky-reviews.vercel.app";
 
-  const baseUrl = "https://northsky-reviews.vercel.app";
+
+export default function robots() {
 
 
   return {
 
+
     rules: [
+
       {
+
         userAgent: "*",
+
         allow: "/",
 
+
         disallow: [
+
           "/api/",
+
           "/admin/",
+
+          "/dashboard/",
+
+          "/private/",
+
         ],
+
       },
+
     ],
+
 
 
     sitemap:
       `${baseUrl}/sitemap.xml`,
 
 
+
     host:
       baseUrl,
 
+
   };
+
 
 }
