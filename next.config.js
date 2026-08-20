4c80328c48858f0062dc385a08a23d761a5424b1
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-08-20 10:53:50 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,30 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
-
-  async redirects() {
-    return [
-      {
-        source: "/category",
-        destination: "/category/ai",
-        permanent: true,
-      },
-      {
-        source: "/categories",
-        destination: "/category/ai",
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
